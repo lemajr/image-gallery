3b523d5677e855c4e4bf7f79f43d081f7e922526
lemajr <erickbale360@gmail.com>
2024-10-25 11:30:25 +0300

    final draft to deploy

--- gallery_project/asgi.py
+++ gallery_project/asgi.py
@@ -14,5 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gallery_project.settings')
 
 application = get_asgi_application()
-
-app = application
