9566af893ce89dbe76ba1e255c2560bc8503dd57
lemajr <erickbale360@gmail.com>
2024-10-25 10:10:48 +0300

    adding app to wsgi

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -71,17 +71,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gallery_project.wsgi.application'
+# WSGI_APPLICATION = 'api.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -114,7 +115,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-WSGI_APPLICATION = 'api.wsgi.app'
 
 
 # Static files (CSS, JavaScript, Images)
